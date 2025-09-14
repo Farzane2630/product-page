0bc336115bb5a5153bfebe46db2defe25c54707e
farzane <farzanekazemi8517@gmail.com>
2025-09-14 20:45:02 +0330

    fix: nav/ axe issues

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const closeMenu = document.querySelector(".close-menu-btn");
 
 menuBtn.addEventListener("click", () => {
   mobileMenu.classList.add("expanded");
+  mobileMenu.setAttribute("aria-expanded", true);
 });
 
 closeMenu.addEventListener("click", () => {
   mobileMenu.classList.remove("expanded");
+    mobileMenu.setAttribute("aria-expanded", false);
+
 });
