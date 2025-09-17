1b5dd04ea6cc41b938096f8fcccdacd399a6685f
farzane <farzanekazemi8517@gmail.com>
2025-09-17 21:03:57 +0330

    feat: previous img btn functions

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const menuBtn = document.querySelector(".menu-btn");
 const mobileMenu = document.querySelector(".main-navigation");
 const closeMenu = document.querySelector(".close-menu-btn");
 
+const productImage = document.querySelector(".product-img");
+const previousBtn = document.getElementById("icon-previous");
+const nextBtn = document.getElementById("icon-next");
+
 menuBtn.addEventListener("click", () => {
   mobileMenu.classList.add("expanded");
   mobileMenu.setAttribute("aria-expanded", true);
@@ -9,6 +13,30 @@ menuBtn.addEventListener("click", () => {
 
 closeMenu.addEventListener("click", () => {
   mobileMenu.classList.remove("expanded");
-    mobileMenu.setAttribute("aria-expanded", false);
+  mobileMenu.setAttribute("aria-expanded", false);
+});
 
+const productImages = [
+  "/images/image-product-1.jpg",
+  "/images/image-product-2.jpg",
+  "/images/image-product-3.jpg",
+  "/images/image-product-4.jpg",
+];
+
+previousBtn.addEventListener("click", () => {
+  
+    const currentImgSrc = productImage.getAttribute("src");
+    const currentImgIndex = productImages.findIndex(
+      (img) => img == currentImgSrc
+    );
+
+    if (currentImgIndex <= 3 && currentImgIndex >= 1) {
+      productImage.setAttribute("src", productImages[currentImgIndex - 1]);
+    } else {
+      productImage.setAttribute("src", productImages[3]);
+    }
+
+});
+nextBtn.addEventListener("click", () => {
+  // setProductImgSrc("+");
 });
